485c74b8055085e3949bc9b9bb74a4bef5be5c2d
baharKaviani <bhr.kaviani@gmail.com>
2021-06-14 18:45:25 +0430

    add: Menu component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div :key = index v-for="index in 10">
+      {{index}}<Menu/>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from "@/components/Menu";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Menu
   }
 }
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>menu</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Menu"
+    }
+</script>
+
+<style scoped>
+
+</style>
